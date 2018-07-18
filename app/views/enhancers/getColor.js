7a709c3123bb9a1aad39f5af189c7365f60ef539
Ramon Boss <ramon.boss@outlook.com>
2018-07-18 09:32:24 +0200

    changed colors

--- app/views/enhancers/getColor.js
+++ app/views/enhancers/getColor.js
@@ -3,6 +3,6 @@ import moment from 'moment'
 export default (date, colors) =>
   date && date.isSameOrBefore(moment(), 'day')
     ? date.isSameOrBefore(moment())
-      ? colors.textColorTodayEvent
-      : colors.textColorPastEvent
+      ? colors.textColorPastEvent
+      : colors.textColorTodayEvent
     : colors.textColorDisabled
